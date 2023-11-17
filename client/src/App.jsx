25bd17184f003aebd8e0505ef5c032c3d48ed1f7
Ikari Vargas <dark.2002ikari@gmail.com>
2023-11-17 16:52:33 -0600

    Agregamos vistas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignPage from "./pages/SignPage.jsx";
 
 function App(){
    return (
-      <div>
-        <h1 className="text-5xl font-bold text-center">
-         Hola Mundo
-        </h1>
-      </div>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<h1>INDEX</h1>} />
+            <Route path="/sign" element={<SignPage />} />
+            <Route path="/user" element={<h1>USER</h1>} />
+            <Route path="/post" element={<h1>POST</h1>} />
+            <Route path="/comment" element={<h1>COMMENT</h1>} />
+            <Route path="/reactions" element={<h1>REACTIONS</h1>} />
+         </Routes>
+      </BrowserRouter>
    );
 }
 
